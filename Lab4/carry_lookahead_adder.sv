de2dcf90511bf98060aaf6f09b2e540c3ca7c374
arkajitd-hub <76401729+arkajitd-hub@users.noreply.github.com>
2020-12-20 11:16:20 -0600

    FPGA/ System Verilog

---
+++ Lab4/carry_lookahead_adder.sv
@@ -0,0 +1,53 @@
+module carry_lookahead_adder
+(
+    input   logic[15:0]     A,
+    input   logic[15:0]     B,
+    output  logic[15:0]     SUM,
+    output  logic           CO,
+	 input 	logic 			 Cin
+	 );
+	 
+	 logic c1,c2,c3;
+	 
+	 look_ahead_4_bits(.A(A[3:0]), .B(B[3:0]), .Cin(Cin), .SUM(SUM[3:0]), .CO(c1));
+	 look_ahead_4_bits(.A(A[7:4]), .B(B[7:4]), .Cin(c1), .SUM(SUM[7:4]), .CO(c2));
+	 look_ahead_4_bits(.A(A[11:8]), .B(B[11:8]), .Cin(c2), .SUM(SUM[11:8]), .CO(c3));
+	 look_ahead_4_bits(.A(A[15:12]), .B(B[15:12]), .Cin(c3), .SUM(SUM[15:12]), .CO(CO));
+
+
+    /* TODO
+     *
+     * Insert code here to implement a CLA adder.
+     * Your code should be completly combinational (don't use always_ff or always_latch).
+     * Feel free to create sub-modules or other files. */
+     
+endmodule
+
+module look_ahead_4_bits(A,B, Cin, SUM, CO);
+input [3:0] A,B;
+input Cin;
+output logic [3:0] SUM;
+output CO;
+
+wire [3:0] p,g,c;
+
+assign p[0] = A[0]^B[0];
+assign p[1] = A[1]^B[1];
+assign p[2] = A[2]^B[2];
+assign p[3] = A[3]^B[3];
+assign g[0] = A[0]&B[0];
+assign g[1] = A[1]&B[1];
+assign g[2] = A[2]&B[2];
+assign g[3] = A[3]&B[3];
+
+assign c[0] = Cin;
+assign c[1] = (g[0] | p[0]&c[0]);
+assign c[2] = (g[1] | p[1]&c[1]);
+assign c[3] = (g[2] | p[2]&c[2]);
+assign CO = (g[3] | p[3]&c[3]);
+assign SUM[0] = p[0] ^ c[0];
+assign SUM[1] = p[1] ^ c[1];
+assign SUM[2] = p[2] ^ c[2];
+assign SUM[3] = p[3] ^ c[3];
+
+endmodule
---
+++ Lab4/carry_select_adder.sv
@@ -0,0 +1,98 @@
+module carry_select_adder
+(
+    input   logic[15:0]     A,
+    input   logic[15:0]     B,
+    output  logic[15:0]     SUM,
+    output  logic           CO
+	 
+	 
+	 
+);
+		
+		logic [2:0] c;
+		
+		ripple_cArry_4Bit cA1(.A(A[3:0]), .B(B[3:0]), .Cin(1'B0), .SUM(SUM[3:0]), .CO(c[0]));
+		cArry_select_4Bit_slice slice1(.A(A[7:4]), .B(B[7:4]), .Cin(c[0]), .SUM(SUM[7:4]), .CO(c[1]));
+		cArry_select_4Bit_slice slice2(.A(A[11:8]), .B(B[11:8]), .Cin(c[1]), .SUM(SUM[11:8]), .CO(c[2]));
+		cArry_select_4Bit_slice slice3(.A(A[15:12]), .B(B[15:12]), .Cin(c[2]), .SUM(SUM[15:12]), .CO(CO));
+     
+endmodule
+
+
+module cArry_select_4Bit_slice
+(
+	input logic[3:0] A,B,
+	input logic Cin,
+	output logic[3:0] SUM,
+	output logic CO
+	
+);
+
+		logic [3:0] s0,s1;
+		logic c0,c1;
+		
+		ripple_cArry_4Bit cA1(.A(A[3:0]), .B(B[3:0]), .Cin(1'B0), .SUM(s0[3:0]), .CO(c0));
+		ripple_cArry_4Bit cA2(.A(A[3:0]), .B(B[3:0]), .Cin(1'B1), .SUM(s1[3:0]), .CO(c1));
+		mux2X1 ms0(.in1(s0[3:0]), .in2(s1[3:0]), .select(Cin), .out(SUM[3:0]));
+		logic_1 L1(.A(c0), .B(c1), .C(Cin), .CO(CO));
+
+endmodule
+		
+module ripple_cArry_4Bit
+(
+	input logic [3:0] A,B,
+	input logic Cin,
+	output logic [3:0] SUM,
+	output logic CO
+
+);
+
+		logic c1,c2,c3;
+		
+		full_Adder_1Bit Bit0(.A(A[0]), .B(B[0]), .Cin(Cin), .SUM(SUM[0]), .CO(c1));
+		full_Adder_1Bit Bit1(.A(A[1]), .B(B[1]), .Cin(c1), .SUM(SUM[1]), .CO(c2));
+		full_Adder_1Bit Bit2(.A(A[2]), .B(B[2]), .Cin(c2), .SUM(SUM[2]), .CO(c3));
+		full_Adder_1Bit Bit3(.A(A[3]), .B(B[3]), .Cin(c3), .SUM(SUM[3]), .CO(CO));
+
+endmodule
+		
+		
+
+module full_Adder_1Bit
+(
+		input logic A,B,Cin,
+		output logic SUM,CO
+		
+);
+
+		assign SUM = A ^ B^ Cin;
+		assign CO = (A&B) | (B&Cin) | (Cin&A);
+
+endmodule
+
+
+module mux2X1(in1,in2,select,out);
+
+
+		input logic [3:0] in1,in2;
+		input logic select;
+		output logic [3:0] out;
+		assign out=(select)?in2:in1;
+
+
+endmodule
+
+
+module logic_1
+(
+	input logic A,B,C,
+	output logic CO
+);
+
+	assign out1 = B & C;
+	assign CO = out1 | A;
+	
+		
+		
+
+endmodule
